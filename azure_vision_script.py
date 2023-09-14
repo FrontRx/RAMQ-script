6724fcd47794f70af763fe9e62a507b82a3f5cc7
josephgcedeno <josephgcedeno@gmail.com>
2023-09-14 15:29:47 +0800

    temp: comment out redundant import and function call

--- azure_vision_script.py
+++ azure_vision_script.py
@@ -5,13 +5,13 @@ from llama_hub.tools.azure_cv.base import AzureCVToolSpec
 import openai
 openai.api_key = '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'
 from llama_index.agent import OpenAIAgent
-import re
-from datetime import datetime
-from llama_hub.tools.azure_cv.base import AzureCVToolSpec
-# Setup OpenAI Agent
-import openai
-openai.api_key = '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'
-from llama_index.agent import OpenAIAgent
+# import re
+# from datetime import datetime
+# from llama_hub.tools.azure_cv.base import AzureCVToolSpec
+# # Setup OpenAI Agent
+# import openai
+# openai.api_key = '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'
+# from llama_index.agent import OpenAIAgent
 
 def get_ramq(image_url):
     cv_tool = AzureCVToolSpec(
@@ -114,36 +114,36 @@ def get_ramq(image_url):
 
 
 # Usage
-image_url = "https://i.ibb.co/4VyHrkV/Screenshot-2023-08-15-at-10-52-28-PM.png"
-try:
-    ramq, last_name, first_name, dob, gender = get_ramq(image_url)
-    print(f"RAMQ: {ramq}")
-    print(f"Last Name: {last_name}")
-    print(f"First Name: {first_name}")
-    print(f"Date of Birth: {dob}")
-    print(f"Gender: {gender}")
-except ValueError as e:
-    print(e)
+# image_url = "https://i.ibb.co/4VyHrkV/Screenshot-2023-08-15-at-10-52-28-PM.png"
+# try:
+#     ramq, last_name, first_name, dob, gender = get_ramq(image_url)
+#     print(f"RAMQ: {ramq}")
+#     print(f"Last Name: {last_name}")
+#     print(f"First Name: {first_name}")
+#     print(f"Date of Birth: {dob}")
+#     print(f"Gender: {gender}")
+# except ValueError as e:
+#     print(e)
 
-# In case the RAMQ is not found, it will print the error message
+# # In case the RAMQ is not found, it will print the error message
 
 
-import argparse
+# import argparse
 
-def main():
-    parser = argparse.ArgumentParser(description='Get RAMQ details from an image URL.')
-    parser.add_argument('image_url', type=str, help='The URL of the image to process.')
-    args = parser.parse_args()
+# def main():
+#     parser = argparse.ArgumentParser(description='Get RAMQ details from an image URL.')
+#     parser.add_argument('image_url', type=str, help='The URL of the image to process.')
+#     args = parser.parse_args()
 
-    try:
-        ramq, last_name, first_name, dob, gender = get_ramq(args.image_url)
-        print(f"RAMQ: {ramq}")
-        print(f"Last Name: {last_name}")
-        print(f"First Name: {first_name}")
-        print(f"Date of Birth: {dob}")
-        print(f"Gender: {gender}")
-    except ValueError as e:
-        print(e)
+#     try:
+#         ramq, last_name, first_name, dob, gender = get_ramq(args.image_url)
+#         print(f"RAMQ: {ramq}")
+#         print(f"Last Name: {last_name}")
+#         print(f"First Name: {first_name}")
+#         print(f"Date of Birth: {dob}")
+#         print(f"Gender: {gender}")
+#     except ValueError as e:
+#         print(e)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
